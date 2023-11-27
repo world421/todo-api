6254f7ca8f0b17e70f791983e6ab3b3debbf9640
kyoungmin <stephen4951@gmail.com>
2023-11-27 14:35:47 +0900

    #4. User 회원가입 컨트롤러

--- src/main/java/com/example/todo/userapi/service/UserService.java
+++ src/main/java/com/example/todo/userapi/service/UserService.java
@@ -21,7 +21,7 @@ public class UserService {
     public UserSignUpResponseDTO create(final UserRequestSignUpDTO dto) {
         String email = dto.getEmail();
 
-        if(userRepository.existsByEmail(email)) {
+        if(isDuplicate(email)) {
             log.warn("이메일이 중복되었습니다. - {}", email);
             throw new RuntimeException("중복된 이메일 입니다.");
         }
@@ -38,7 +38,9 @@ public class UserService {
 
     }
 
-
+    public boolean isDuplicate(String email) {
+        return userRepository.existsByEmail(email);
+    }
 }
 
 
